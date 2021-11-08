a0a70492bbdff5e361f5c20d7511504b6b2f003c
mausam7 <39566442+mausam7@users.noreply.github.com>
2021-11-08 11:06:14 +0545

    form changed

--- src/components/employeeForm.jsx
+++ src/components/employeeForm.jsx
@@ -1,200 +1,125 @@
-import { UploadOutlined} from '@ant-design/icons';
-import { Form, Input, Button, Select, DatePicker, Image, Upload, message } from "antd";
-import React, { Component } from "react";
-const { Option } = Select;
+import React from "react";
 
-function getBase64(img, callback) {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-}
-
-class EmployeeForm extends Component {
-  state = {
-       loading: false,
-  };
-
-  handleChange = info => {
-    this.setState({
-      imageUrl:info.file.originFileObj,
-      loading: false,
-    })
-  };
-
-  onFinish = (values) => {
-    console.log("Success:", values);
-    if (!localStorage.getItem("count")) {
-      localStorage.setItem("count", 0);
-    }
-    const count = localStorage.getItem("count");
-    const employee_number = parseInt(count) + 1;
-    localStorage.setItem("count", employee_number);
-    localStorage.setItem("employee" + employee_number, employee_number);
-    for (const name in values) {
-      localStorage.setItem(name + employee_number, values[name]);
-    }
-    window.location = "/employee-table";
-
-  };
-
-  onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select style={{ width: 80 }}>
-        <Option value="977">+977</Option>
-        <Option value="1">+1</Option>
-        <Option value="99">+99</Option>
-      </Select>
-    </Form.Item>
-  );
-  render() {
-    const { imageUrl } = this.state;
-    return (<div>
-
-      <h1 style={{ textAlign: "center" }}>Employee Form</h1>
-      <React.Fragment>
-        <Form
-          name="basic"
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ prefix: "977", remember: true }}
-          onFinish={this.onFinish}
-          onFinishFailed={this.onFinishFailed}
-          autoComplete="off"
+const EmployeeForm = () => {
+    return(
+        <div class="container bg-light">
+      <div class="row py-5 justify-content-center">
+        <div
+          class="registrationform col-lg-5"
+          style={{position: "relative", paddingRight: "100px", paddingBottom: "50px"}}
         >
-          <Form.Item
-            label="First Name"
-            name="firstname"
-            rules={[
-              { required: true, message: "Please input your firstname!" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Last Name"
-            name="lastname"
-            rules={[
-              { required: true, message: "Please input your lastname!" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="gender"
-            label="Gender"
-            rules={[{ required: true, message: "Please select gender!" }]}
-          >
-            <Select placeholder="Select your gender">
-              <Option value="male">Male</Option>
-              <Option value="female">Female</Option>
-              <Option value="other">Other</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name="DOB"
-            label="D.O.B."
-            rules={[{ required: true, message: "Please select your Date of Birth" }]}
-          >
-            <DatePicker />
-          </Form.Item>
-
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not valid E-mail!",
-              },
-              {
-                required: true,
-                message: "Please input your E-mail!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="phone"
-            type="number"
-            label="Phone Number"
-            rules={[
-              { required: true, message: "Please input your phone number!" },
-            ]}
-          >
-            <Input addonBefore={this.prefixSelector} style={{ width: "100%" }} />
-          </Form.Item>
-
-          <Form.Item
-            name="country"
-            label="Country"
-            rules={[{ required: true, message: "Please select country!" }]}
-          >
-            <Select placeholder="Select your country">
-              <Option value="Nepal">Nepal</Option>
-              <Option value="Chaina">Chaina</Option>
-              <Option value="USA">USA</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name="designation"
-            label="Designation"
-            rules={[
-              { required: true, message: "Please select designation!" },
-            ]}
-          >
-            <Select placeholder="Select your designation">
-              <Option value="QA">QA</Option>
-              <Option value="dev">Developer</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name="start-date"
-            label="Start Date"
-            rules={[{ required: true, message: "Please select start date!" }]}
-          >
-            <DatePicker />
-          </Form.Item>
-          <Form.Item
-            name="photo"
-            label="Photo"
-            rules={[{ required: true, message: "Please upload photo!" }]}
-          >
-            <Upload
-        showUploadList={false}
-        onChange={this.handleChange}>
-           <Button icon={<UploadOutlined />}>Click to Upload</Button>
-          </Upload>
-          </Form.Item>
-
-          
-          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-          <Image
-            src={imageUrl?URL.createObjectURL(imageUrl):"https://www.fullform-shortform.com/wp-content/uploads/2021/05/Welcome-Short-Form.png"} alt="photo" style={{ width: '300px', height: '250px' }}
-          />
-          </Form.Item>
-          
-          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </React.Fragment>
+          <h3>Employee Form</h3>
+          <form action="#" method="post">
+            <div class="mt-3 mb-3">
+              <label for="Firstname" class="form-label">Firstname</label>
+              <input type="name" class="form-control" id="Firstname" required />
+            </div>
+            <div class="mt-3 mb-3">
+              <label for="Lastname" class="form-label">Lastname</label>
+              <input type="name" class="form-control" id="Lastname" required />
+            </div>
+            <label for="gender" class="form-label">Gender</label>
+            <div class="row g-3 align-items-center mb-3">
+              <div class="form-check col-lg-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
+                  checked
+                />
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Male
+                </label>
+              </div>
+              <div class="form-check col-lg-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault2"
+                />
+                <label class="form-check-label" for="flexRadioDefault2">
+                  Female
+                </label>
+              </div>
+              <div class="form-check col-lg-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault3"
+                />
+                <label class="form-check-label" for="flexRadioDefault3">
+                  Other
+                </label>
+              </div>
+            </div>
+            <div class="mb-3">
+              <label for="Phone" class="form-label">Phone Number</label>
+              <input
+                type="number"
+                class="form-control"
+                id="Phone"
+                aria-describedby="phonelHelp"
+                required
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label"
+                >Email Address</label
+              >
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputCountry" class="form-label"
+                >Country</label
+              >
+              <select name="country" id="country" class="form-control" required>
+                <option value="">Select country</option>
+                <option value="nepal">Nepal</option>
+                <option value="usa">Chaina</option>
+                <option value="usa">USA</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputDesignation" class="form-label"
+                >Designation</label
+              >
+              <select
+                name="Designation"
+                id="Designation"
+                class="form-control"
+                required
+              >
+                <option value="">Select designation</option>
+                <option value="qa">QA</option>
+                <option value="ba">Developer</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="cexampleInputStartDate" class="form-label"
+                >Start-Date</label
+              >
+              <input
+                type="date"
+                class="form-control"
+                id="cexampleInputStartDate"
+                required
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
     );
-  }
+}
 
-};
-
-export default EmployeeForm;
+export default  EmployeeForm;
